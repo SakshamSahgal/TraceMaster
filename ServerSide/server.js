7ba1680a52485d47b4be0b30b0ba12217ca50f90
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2023-10-25 21:20:52 +0530

    added geo-location

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -5,19 +5,19 @@ const useragent = require('express-useragent');
 const fs = require('fs');
 const axios = require('axios');
 
-app.use(express.static(path.join(__dirname,"..","ClientSide","Static"))); //telling that my webapp will be using the files in the ClientSide/Static folder for static js files
+app.use(express.static(path.join(__dirname,"..","ClientSide","Static")));   //telling that my webapp will be using the files in the ClientSide/Static folder for static js files
+app.use(useragent.express());                                               // use the useragent middleware to parse useragent header
 
-app.set('trust proxy', true); // Enable "trust proxy" to get the client's IP address through proxy headers
 
+app.set('trust proxy', true);       // Enable "trust proxy" to get the client's IP address through proxy headers [setting this makes the load balancer to forward the client's IP address in the X-Forwarded-For header instead of loopback address]
 app.set('view engine', 'ejs');      // set the view engine to ejs
-app.use(useragent.express());       // use the useragent middleware to parse useragent header
 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     
     // Access user agent information from req.useragent
     const userAgentInfo = req.useragent;
@@ -28,9 +28,8 @@ app.get('/', (req, res) => {
         osInfo: getOS(userAgentInfo.os), 
         ip : req.ip,
         deviceTypeInfo : getDeviceType(userAgentInfo),
+        locationData : await getUserLocation(req.ip)
     }
-    getUserLocation(req.ip)
-    
     res.render(path.join(__dirname,"..","ClientSide","index.ejs"),template)
 })
 
@@ -49,7 +48,7 @@ function getBrowser(browser) {
     else
         browserInfo.logo = path.join("GUI","Browser","default.png")
     
-        return browserInfo
+    return browserInfo;
 }
 
 function getOS(os) {
@@ -68,7 +67,7 @@ function getOS(os) {
       
       
     if(os.toLowerCase().includes("windows"))
-        osInfo.logo = path.join("GUI","OS","windows.png")
+        osInfo.logo = path.join("GUI","OS","Windows.png")
     else if (linuxDistributions.some(dist => os.toLowerCase().includes(dist)))
         osInfo.logo = path.join("GUI", "OS", "Linux.png");
     else if(os.toLowerCase().includes("mac"))
@@ -80,7 +79,7 @@ function getOS(os) {
     else
         osInfo.logo = path.join("GUI","OS","Default.png")    
     
-        return osInfo;
+    return osInfo;
 }
 
 function getDeviceType(userAgentInfo) {
@@ -103,7 +102,6 @@ function getDeviceType(userAgentInfo) {
     }
 
     return DeviceTypeInfo;
-    
 }
 
 const getUserLocation = async (ip) => {
@@ -116,9 +114,11 @@ const getUserLocation = async (ip) => {
         const response = await axios.get(`https://ipinfo.io/${ipv4Address}/json`);
         const locationData = response.data;
         console.log(locationData)
+        return locationData;
         
     } catch (error) {
         // console.error(error);
         console.log("req failed")
+        return null;
     }
 };
